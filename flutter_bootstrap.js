1c2167f889dc8cbd9e28ce258cd7a4164a8ed924
TheSoundOfGoodBye <karbehemoth@gmail.com>
2026-03-25 22:58:53 +0900

    deploy: gh-pages v2.1 (Elderly UX)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3560262547" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1900062510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
